6a6d8a841f2e2b347d54b7ea16aa65b7913a0e90
jinusong14@gmail.com <jinusong14@gmail.com>
2024-09-11 21:16:07 +0900

    [송진우] - 최초 프로젝트 생성

---
+++ src/test/java/com/jinu/jinugram/JinugramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinu.jinugram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinugramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
